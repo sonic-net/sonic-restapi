227798d6ef8020ef54a7265ef711f2d129d8daea
Ubuntu <weixchen@weixchen-dev.ugsisdkixl2ujfji5zhdm5fjih.xx.internal.cloudapp.net>
2019-07-03 00:51:15 +0000

    update the latest libthrift package and make cxx complier

--- MSEE/copy.sh
+++ MSEE/copy.sh
@@ -1,6 +1,6 @@
 set -e
 
-SDIR=/data/sonic_build/sonic-buildimage/target/debs
+SDIR=/home/debs/debs/stretch
 cp $SDIR/libhiredis0.13_0.13.3-2_amd64.deb debs
 cp $SDIR/libhiredis-dev_0.13.3-2_amd64.deb debs
 cp $SDIR/libnl-3-200_3.2.27-2_amd64.deb debs
@@ -8,4 +8,4 @@ cp $SDIR/libnl-genl-3-200_3.2.27-2_amd64.deb debs
 cp $SDIR/libnl-route-3-200_3.2.27-2_amd64.deb debs
 cp $SDIR/libswsscommon_1.0.0_amd64.deb debs
 cp $SDIR/libswsscommon-dev_1.0.0_amd64.deb debs
-cp $SDIR/libthrift-0.9.3_0.9.3-2_amd64.deb debs
+cp $SDIR/libthrift-0.11.0_0.11.0-4_amd64.deb debs
