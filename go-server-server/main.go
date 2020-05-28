436f703f3c3a2b71977655f2ca53b5bc27021669
Sumukha Tumkur Vani <stumkurv@microsoft.com>
2020-05-28 05:35:40 +0000

    Handle Signals efficiently

--- go-server-server/main.go
+++ go-server-server/main.go
@@ -15,11 +15,11 @@ import (
     "crypto/tls"
     "crypto/x509"
     "io/ioutil"
-    "time"
     "os"
     "os/signal"
     "syscall"
     "context"
+    "sync"
 )
 
 func StartHttpServer(handler http.Handler) {
@@ -27,8 +27,8 @@ func StartHttpServer(handler http.Handler) {
     log.Fatal(http.ListenAndServe(":8090", handler))
 }
 
-
-func StartHttpsServer(handler http.Handler) {
+func StartHttpsServer(handler http.Handler, messenger <-chan int, wgroup *sync.WaitGroup) {
+    defer wgroup.Done()
     clientCert, err := ioutil.ReadFile(*sw.ClientCertFlag)
     if err != nil {
         log.Fatalf("error: couldn't open client cert file, %s", err)
@@ -49,39 +49,56 @@ func StartHttpsServer(handler http.Handler) {
     }
 
     tlsConfig.BuildNameToCertificate()
+    for {
+        server := &http.Server{
+            Addr:      ":8081",
+            Handler:   handler,
+            TLSConfig: tlsConfig,
+        }
 
-    server := &http.Server{
-        Addr:      ":8081",
-        Handler:   handler,
-        TLSConfig: tlsConfig,
+        log.Printf("info: https endpoint started")
+
+        // Listening should happen in a go-routine to prevent blocking
+        go func() {
+            if err := server.ListenAndServeTLS(*sw.ServerCertFlag, *sw.ServerKeyFlag); err != nil {
+                log.Println(err)
+            }
+        }()
+
+        value := <-messenger
+        log.Printf("info: HTTPS Signal received. Shutting down...")
+        if err := server.Shutdown(context.Background()); err != nil {
+            log.Printf("trace: HTTPS server Shutdown: %v", err)
+        } else {
+            log.Printf("info: HTTPS Server shutdown successful!")
+        }
+        switch value {
+        case 0:
+            log.Printf("info: Terminating...")
+            return
+        }
     }
+}
 
-    log.Printf("info: https endpoint started")
-
-    // Listening should happen in a go-routine to prevent blocking
-    go func() {
-        log.Fatal(server.ListenAndServeTLS(*sw.ServerCertFlag, *sw.ServerKeyFlag))
-    }()
-
+func signal_handler(messenger chan<- int, wgroup *sync.WaitGroup) {
+    defer wgroup.Done()
     sigchannel := make(chan os.Signal, 1)
     signal.Notify(sigchannel,
         syscall.SIGTERM,
+        syscall.SIGKILL,
         syscall.SIGQUIT)
 
     <-sigchannel
-    log.Printf("info: Signal received. Shutting down...")
-    if err := server.Shutdown(context.Background()); err != nil {
-        log.Printf("trace: HTTP server Shutdown: %v", err)
-    } else {
-        log.Printf("info: Server shutdown successful!")
-    }
-    os.Exit(0)
+    messenger <- 0
+    return
 }
 
 func main() {
     iniflags.Parse()
 
     sw.InitLogging()
+    var wgroup sync.WaitGroup
+    var messenger = make(chan int, 1)
 
     log.Printf("info: server started")
 
@@ -97,13 +114,13 @@ func main() {
     }
 
     if (*sw.HttpsFlag) {
-        go StartHttpsServer(router)
+        wgroup.Add(1)
+        go StartHttpsServer(router, messenger, &wgroup)
     }
 
-    // infinite loop to keep Goroutines running
-    for {
-        time.Sleep(1 * time.Second)
-    }
+    wgroup.Add(1)
+    go signal_handler(messenger, &wgroup)
+    wgroup.Wait()
 }
 
 
