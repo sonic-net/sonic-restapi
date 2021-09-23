ba2a558cdb93499c017beeb7cbfa2a9dd230e0d3
shilongliu <shilongliu@microsoft.com>
2021-09-23 10:22:03 +0800

    [CI] Set up azure pipeline

--- README.md
+++ README.md
@@ -4,21 +4,15 @@
 This is a configuration agent which exposes HTTPS endpoints to perform dynamic network configuration on switches running SONiC. It restful API server is `go-server-server`
 
 ## Getting Started
-### Fetch Dependencies
-  1. `mkdir debs; cd debs`
-  2. `sudo wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/vs/job/buildimage-vs-all/lastSuccessfulBuild/artifact/target/debs/*zip*/debs.zip`
-  3. `sudo unzip debs.zip`
-  
 ### Build Rest-API
-  1. Modify `copy.sh` SDIR path to point to your downloaded debs from the previous step
-  2. Execute ./build.sh
-  3. The above should generate 2 Docker images, 1 which is used for local development on your VM and 1 which is used for deployment on a TOR
-  4. Run `docker images` to check if rest-api dockers were generated <br/>
+  1. Execute ./build.sh
+  2. The above should generate 2 Docker images, 1 which is used for local development on your VM and 1 which is used for deployment on a TOR
+  3. Run `docker images` to check if rest-api dockers were generated <br/>
       		<pre>`REPOSITORY                     TAG                 IMAGE ID            CREATED             SIZE` <br/>
 		      `rest-api-image                 latest              d2815fcb7356        2 days ago          222MB` <br/>
           `rest-api-image-test_local      latest              e62219a0bae2        2 days ago          222MB`</pre>
-  5. `rest-api-image-test_local` is for local testing on a dev VM and `rest-api-image` is for TOR testing/deployment
-  6. The production image is also stored into a compressed archive `rest-api-image.gz`
+  4. `rest-api-image-test_local` is for local testing on a dev VM and `rest-api-image` is for TOR testing/deployment
+  5. The production image is also stored into a compressed archive `rest-api-image.gz`
 ### Running Rest-API container
 #### Run Rest-API container locally on a VM and execute unit tests
   1. `docker run -d --rm -p8090:8090 -p6379:6379 --name rest-api --cap-add NET_ADMIN --privileged -t rest-api-image-test_local:latest`
--- copy.sh
+++ copy.sh
@@ -1,11 +1,12 @@
 set -e
 
-SDIR=/home/debs/debs/stretch
-cp $SDIR/libhiredis0.13_0.13.3-2_amd64.deb debs
-cp $SDIR/libhiredis-dev_0.13.3-2_amd64.deb debs
-cp $SDIR/libnl-3-200_3.2.27-2_amd64.deb debs
-cp $SDIR/libnl-genl-3-200_3.2.27-2_amd64.deb debs
-cp $SDIR/libnl-route-3-200_3.2.27-2_amd64.deb debs
-cp $SDIR/libswsscommon_1.0.0_amd64.deb debs
-cp $SDIR/libswsscommon-dev_1.0.0_amd64.deb debs
-cp $SDIR/libthrift-0.11.0_0.11.0-4_amd64.deb debs
+wget -O debs/libhiredis0.14_0.14.0-3~bpo9+1_amd64.deb  'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target%2Fdebs%2Fbuster%2Flibhiredis0.14_0.14.0-3~bpo9%2B1_amd64.deb'
+wget -O debs/libhiredis-dev_0.14.0-3~bpo9+1_amd64.deb  'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target%2Fdebs%2Fbuster%2Flibhiredis-dev_0.14.0-3~bpo9%2B1_amd64.deb'
+wget -O debs/libnl-3-200_3.5.0-1_amd64.deb 'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target/debs/buster/libnl-3-200_3.5.0-1_amd64.deb'
+wget -O debs/libnl-3-dev_3.5.0-1_amd64.deb 'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target/debs/buster/libnl-3-dev_3.5.0-1_amd64.deb'
+wget -O debs/libnl-genl-3-200_3.5.0-1_amd64.deb  'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target/debs/buster/libnl-genl-3-200_3.5.0-1_amd64.deb'
+wget -O debs/libnl-route-3-200_3.5.0-1_amd64.deb  'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target/debs/buster/libnl-route-3-200_3.5.0-1_amd64.deb'
+wget -O debs/libnl-nf-3-200_3.5.0-1_amd64.deb  'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target/debs/buster/libnl-nf-3-200_3.5.0-1_amd64.deb'
+wget -O debs/libthrift-0.11.0_0.11.0-4_amd64.deb 'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target/debs/buster/libthrift-0.11.0_0.11.0-4_amd64.deb'
+wget -O debs/libswsscommon_1.0.0_amd64.deb 'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target%2Fdebs%2Fbuster%2Flibswsscommon_1.0.0_amd64.deb'
+wget -O debs/libswsscommon-dev_1.0.0_amd64.deb 'https://sonic-build.azurewebsites.net/api/sonic/artifacts?branchName=master&platform=vs&target=target%2Fdebs%2Fbuster%2Flibswsscommon-dev_1.0.0_amd64.deb'
