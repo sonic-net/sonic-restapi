527caf985dafa6ae856a680775516cd84b11a39e
Sumukha Tumkur Vani <sumukhatv@outlook.com>
2021-10-01 20:24:10 +0000

    Increase timeout

--- files/server_kill.py
+++ files/server_kill.py
@@ -4,7 +4,7 @@ import os, signal
 from subprocess import check_output
 import time
 
-time.sleep(3*60)
+time.sleep(10*60)
 
 pid = int(check_output(["pidof", "go-server-server.test"]))
 os.kill(pid, signal.SIGQUIT)
