0c7903f358d69b12f7005f78bb4d23487ade4123
Sumukha Tumkur Vani <sumukhatv@outlook.com>
2022-06-21 00:51:50 +0000

    Add tests

--- test/test_restapi.py
+++ test/test_restapi.py
@@ -795,6 +795,28 @@ class TestRestApiPositive:
         j = json.loads(r.text)
         assert sorted(j) == sorted(routes)
 
+        # Endpoint Monitor optional arg
+        route['vnid'] = 5000
+        route['endpoint_monitor'] = '100.3.152.32,200.3.152.32'
+        route['cmd'] = 'add'
+        r = restapi_client.patch_config_vrouter_vrf_id_routes("vnet-guid-1", [route])
+        assert r.status_code == 204
+        route_table = db.hgetall(ROUTE_TUN_TB + ':' + VNET_NAME_PREF +str(1)+':'+route['ip_prefix'])
+        assert route_table == {b'endpoint' : route['nexthop'],
+                                       b'vni': str(route['vnid']), 
+                                       b'mac_address' : route['mac_address'],
+                                       b'endpoint_monitor': route['endpoint_monitor']
+                                      }
+        del route['cmd']
+        routes = list()
+        routes.append(route)
+        routes.append({'nexthop': '', 'ip_prefix': '10.1.1.0/24', 'ifname': 'Vlan2'})
+        r = restapi_client.get_config_vrouter_vrf_id_routes("vnet-guid-1")
+        assert r.status_code == 200
+        j = json.loads(r.text)
+        assert sorted(j) == sorted(routes)
+        del route['endpoint_monitor']
+
         # Weight optional arg
         route['vnid'] = 5000
         route['weight'] = '20,10'
