4736618593c9b1c6f01eb862135ec2f3d7901314
Sumukha Tumkur Vani <sumukhatv@outlook.com>
2022-07-29 02:01:16 +0000

    Check for ip prefix length

--- go-server-server/go/persistent.go
+++ go-server-server/go/persistent.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "github.com/go-redis/redis/v7"
     "log"
+    "net"
     "strconv"
     "strings"
     "swsscommon"
@@ -491,4 +492,12 @@ func generateDBTableKey(separator string, vars ...string) (string) {
          }
     }
     return buf.String()
+}
+
+func isV4orV6(ipaddr string) (int) {
+    ip, _ := net.ParseIP(string)
+    if ip.To4() != nil {
+        return 4
+    }
+    return 6
 }
