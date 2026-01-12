dbdd26c4386eac2bd99eb6cc16e0f1cb02a5fbb8
Mahdi Ramezani <mramezani@microsoft.com>
2026-01-12 23:25:59 +0000

    Added unit tests for client cert authentication.
    Signed-off-by: Mahdi Ramezani <mramezani@microsoft.com>

--- go-server-server/go/auth.go
+++ go-server-server/go/auth.go
@@ -7,14 +7,14 @@ import (
 )
 
 func CommonNameMatch(r *http.Request) bool {
-    //FIXME : in the authentication of client certificate,  after the certificate chain is validated by 
+    // During client cert authentication, after the certificate chain is validated by
     // TLS, here we will futher check if at least one of the common names in the end-entity certificate
-	// matches one of the trusted common names of the server config. A more strict check may be added here later.
+	// matches one of the trusted common names of the server config.
 	for _, peercert := range r.TLS.PeerCertificates {
 		commonName := peercert.Subject.CommonName
 		for _, name := range trustedCertCommonNames {
 			if strings.HasPrefix(name, "*") {
-				// wildcard common name match
+				// wildcard common name matching
 				domain := name[1:]  //strip "*"
 				if strings.HasSuffix(commonName, domain) {
 					log.Printf("info: CommonName %s in the client cert matches trusted wildcard common name %s", commonName, name)
