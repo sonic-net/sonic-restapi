5c3f4b0bfcb34f5ea892f12aad2a9b2168a9652c
mramezani95 <mramezani@microsoft.com>
2026-01-15 15:56:01 -0800

    Changing the wildcard matching logic for client cert common names (#175)
    Microsoft ADO ID: 36341347
    
    PR #168 added support for wildcard matching between client cert CNs and trusted CNs. This PR changes the wildcard matching logic based on a discussion with @qiluo-msft and @prsunny.
    
    Changes:
    
    Wildcard patterns starting with * but not *. (e.g., *example.sonic) are no longer valid. These patterns are skipped during the matching.
    *. is not a valid trusted CN and will be skipped if present.
    * must match one or more characters. For example, *.example.sonic will not match .example.sonic.
    Note 1: Comparison is always case sensitive, but * can match both upper and lower case characters. For example, *.example.sonic matches TEST.example.sonic, but it does not match TEST.EXAMPLE.SONIC.
    
    Note 2: Multi-level subdomain matching is allowed for wildcard patterns. For example, *.example.sonic matches test.example.sonic, sub.test.example.sonic, one.sub.test.example.sonic, and so on.

--- go-server-server/go/auth.go
+++ go-server-server/go/auth.go
@@ -7,26 +7,41 @@ import (
 )
 
 func CommonNameMatch(r *http.Request) bool {
-    // During client cert authentication, after the certificate chain is validated by
-    // TLS, here we will further check if at least one of the common names in the end-entity certificate
-	// matches one of the trusted common names of the server config.
-	for _, peercert := range r.TLS.PeerCertificates {
-		commonName := peercert.Subject.CommonName
-		log.Printf("info: CommonName in the client cert: %s", commonName)
-		for _, name := range trustedCertCommonNames {
-			if strings.HasPrefix(name, "*") {
+	// During client cert authentication, after the certificate chain is validated by
+	// TLS, here we will further check if at least one of the common names in the end-entity certificate
+	// matches one of the trusted common names in the server config.
+
+	for _, name := range trustedCertCommonNames {
+		is_wildcard := false
+		domain := name
+		if strings.HasPrefix(name, "*.") {
+			if len(name) < 3 {
+				log.Printf("warning: Skipping invalid trusted common name %s", name)
+				continue;
+			}
+			is_wildcard = true
+			domain = name[1:]  //strip "*" but keep the "." at the beginning
+		} else if strings.HasPrefix(name, "*") {
+			log.Printf("warning: Skipping invalid trusted common name %s", name)
+			continue;
+		}
+		for _, peercert := range r.TLS.PeerCertificates {
+			commonName := peercert.Subject.CommonName
+			if is_wildcard {
 				// wildcard common name matching
-				domain := name[1:]  //strip "*"
-				if strings.HasSuffix(commonName, domain) {
-					log.Printf("info: CommonName %s in the client cert matches trusted wildcard common name %s", commonName, name)
+				if len(commonName) > len(domain) && strings.HasSuffix(commonName, domain) {
+					log.Printf("info: Wildcard match between common name %s in the client cert and trusted common name %s", commonName, name)
 					return true;
 				}
-			} else if commonName == name {
-				return true;
+			} else {
+				if commonName == name {
+					log.Printf("info: Exact match with trusted common name %s", name)
+					return true;
+				}
 			}
 		}
 	}
 
-    log.Printf("error: Authentication Fail! None of the CommonNames in the client cert match any of the trusted common names")
-    return false;
+	log.Printf("error: Authentication Fail! None of the common names in the client cert match any of the trusted common names")
+	return false;
 }
