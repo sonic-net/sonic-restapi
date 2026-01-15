ab03476dd788f7c7abecad4df3b14a8320cc0478
Sonic Build Admin <sonicbld@microsoft.com>
2026-01-15 22:26:54 +0000

    Adding support for wildcard CN matching during client cert authentication
    Microsoft ADO ID: 36341347
    
    This PR enables the restapi container (acting as a REST API server) to match Common Names (CNs) provided in the client certificate against trusted wildcard CNs during authentication.
    **Note 1:** Exact matching is still supported.
    **Note 2:** `*` can only appear at the beginning of a trusted CN.
    
    Examples:
    `*.example.com` matches `one.example.com` and `two.one.example.com`, but it does not match `example.com` or `example.com.edu` (wildcard matching).
    `example.com` only matches `example.com` (exact matching).
    
    Unit tests are also included in this PR to verify the expected behavior. For unit tests, trusted CNs are `test.client.restapi.sonic`, `*.example.sonic`, and `*test.sonic`. For each unit test, we generate a new client certificate and sign it with the self-signed certificate under `cert/client/`.

--- go-server-server/go/auth.go
+++ go-server-server/go/auth.go
@@ -1,24 +1,32 @@
 package restapi
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
+	"strings"
 )
 
 func CommonNameMatch(r *http.Request) bool {
-    //FIXME : in the authentication of client certificate,  after the certificate chain is validated by 
-    // TLS, here we will futher check if the common name of the end-entity certificate is in the trusted 
-	// common name list of the server config. A more strict check may be added here later.
+    // During client cert authentication, after the certificate chain is validated by
+    // TLS, here we will further check if at least one of the common names in the end-entity certificate
+	// matches one of the trusted common names of the server config.
 	for _, peercert := range r.TLS.PeerCertificates {
 		commonName := peercert.Subject.CommonName
+		log.Printf("info: CommonName in the client cert: %s", commonName)
 		for _, name := range trustedCertCommonNames {
-			if commonName == name {
+			if strings.HasPrefix(name, "*") {
+				// wildcard common name matching
+				domain := name[1:]  //strip "*"
+				if strings.HasSuffix(commonName, domain) {
+					log.Printf("info: CommonName %s in the client cert matches trusted wildcard common name %s", commonName, name)
+					return true;
+				}
+			} else if commonName == name {
 				return true;
 			}
 		}
-		log.Printf("info: CommonName in the client cert: %s", commonName)
 	}
 
-    log.Printf("error: Authentication Fail! None of the CommonNames in the client cert are found in trusted common names")
+    log.Printf("error: Authentication Fail! None of the CommonNames in the client cert match any of the trusted common names")
     return false;
 }
